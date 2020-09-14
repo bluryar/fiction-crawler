c0edc7d5cd9c0ab00b6b092efddddc65b8e4634f
e <ljkblury@outlook.com>
2020-09-14 17:14:51 +0800

    【FIX】 修正因数据库建立连接时实体路径引起的错误

--- src/DbConnect.ts
+++ src/DbConnect.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata';
 
 import { createConnection } from 'typeorm';
 import configs from '../dbconfig';
+import { Book, Chapter } from './entity';
 
 export const getConnectionByEnv = () => {
   const envStr = process.env.NODE_ENV || 'dev';
-  
+
   const { host, port, username, password, database } = configs[envStr];
   return createConnection({
     type: 'mariadb',
-    entities: [__dirname + '/entity/*.ts'],
+    entities: [Book, Chapter],
     synchronize: true,
     host,
     port,
